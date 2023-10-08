473794a0be71c8a6b3b54ca54af82022fa3c7096
Leonardo Jose <ljcl79@gmail.com>
2023-10-08 08:47:24 -0300

    Proyecto Final

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header.jsx'
+import Nav from './components/Nav/Nav.jsx'
+import Banner from './components/Banner/Banner.jsx'
+
+function App() {
+  
+  return (
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <Banner></Banner>
+    </>
+  )
+}
+
+export default App
